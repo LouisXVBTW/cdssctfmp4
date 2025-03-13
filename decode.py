f90fb7300df62330c535379cc80ebd5b83fcb830
Louis O'Regan <louisoregan.2001@gmail.com>
2025-03-13 20:15:39 +0000

    idk what i changed

--- decode.py
+++ decode.py
@@ -4,31 +4,49 @@ from PIL import Image
 import numpy as np
 import csv
 import time
-img=mpimg.imread('out-img/frame0.jpg')
-print(type(img))
-print(img.flatten())
 
-with open('decoded', 'w') as f:
-    csv.writer(f).writerow(img.flatten())
-
+import cv2
+vidcap = cv2.VideoCapture('output.mp4')
+success,image = vidcap.read()
+countFrame = 0
+while success:
+  cv2.imwrite("out-img/frame%d.jpg" % countFrame, image)     # save frame as JPEG file      
+  success,image = vidcap.read()
+  print('Read a new frame: ', success)
+  countFrame += 1
 binary = []
-tmp = []
-count = 0
-for i in img.flatten():
-    if count%12 == 0:
-        #print (len(tmp))
-        #print(tmp)
-        #print ("NEXT!!")
-        
-        #time.sleep(0.5)
-        if 5 <= sum(tmp) <= 150:
-            binary.append(0)
-        if 160 <= sum(tmp) <= 3060:
-            binary.append(1)
-        tmp = []
-    tmp.append(int(str(i)))
-    count += 1
+for i in range(0,countFrame):
+    fname = 'frame'+str(i)+'.jpg'
+    img=mpimg.imread('out-img/frame0.jpg')
+    print(type(img))
+    print(img.flatten())
 
-with open('output.txt', 'w') as f:
-    csv.writer(f).writerow(binary)
+    
 
+    
+    tmp = []
+    count = 0
+    for i in img.flatten():
+        if count%12 == 0:
+            #print (len(tmp))
+            #print(tmp)
+            #print ("NEXT!!")
+            
+            #time.sleep(0.5)
+            if 5 <= sum(tmp) <= 150:
+                binary.append(1)
+            if 160 <= sum(tmp) <= 3060:
+                binary.append(0)
+            tmp = []
+        tmp.append(int(str(i)))
+        count += 1
+
+    #with open('output1.txt', 'wb') as f:
+    #    csv.writer(f).writerow(binary)
+print (type(binary))
+
+binary_str = "".join(map(str, binary))
+byte_array = bytearray(int(binary_str[i:i+8], 2) for i in range(0, len(binary_str), 8))
+
+with open("output2", "wb") as f:
+    f.write(byte_array)
